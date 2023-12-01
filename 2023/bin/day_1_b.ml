25abcd6bb9e4f49459ffac37648933101e42e62c
Sonicfury <theo.lambrt@gmail.com>
2023-12-01 22:57:23 +0100

    wip

--- 2023/bin/day_1_b.ml
+++ 2023/bin/day_1_b.ml
@@ -3,24 +3,24 @@ open Core
 let input: string = In_channel.read_all "inputs/day_1.txt" 
 
 let replacement_map = [
-    "one", "1";
-    "two", "2";
-    "three", "3";
-    "four", "4";
-    "five", "5";
-    "six", "6";
-    "seven", "7";
-    "eight", "8";
-    "nine", "9";
-    "1", "1";
-    "2", "2";
-    "3", "3";
-    "4", "4";
-    "5", "5";
-    "6", "6";
-    "7", "7";
-    "8", "8";
-    "9", "9";
+  "one", "1";
+  "two", "2";
+  "three", "3";
+  "four", "4";
+  "five", "5";
+  "six", "6";
+  "seven", "7";
+  "eight", "8";
+  "nine", "9";
+  "1", "1";
+  "2", "2";
+  "3", "3";
+  "4", "4";
+  "5", "5";
+  "6", "6";
+  "7", "7";
+  "8", "8";
+  "9", "9";
 ]
 
 let rec replace_substring str start_pos direction nb_replacements =
@@ -29,39 +29,39 @@ let rec replace_substring str start_pos direction nb_replacements =
     str
   else
     let rec find_and_replace = function
-      | [] -> replace_substring str (start_pos + direction) direction nb_replacements
+      | [] -> replace_substring str (start_pos + direction) direction (nb_replacements + 1)
       | (pattern, replacement)::rest ->
         let pattern_len = String.length pattern in
         let match_start_pos =
-            if direction = 1 then
-                start_pos
-            else 
-                start_pos - pattern_len + 1
-            in
-        if start_pos + (pattern_len * direction) <= len &&
-           String.sub str match_start_pos pattern_len = pattern then
+          if direction = 1 then
+            start_pos
+          else
+            start_pos - pattern_len + 1
+        in
+        let substr = String.sub str ~pos:match_start_pos ~len:pattern_len in
+        let is_length_ok = start_pos + (pattern_len * direction) <= len in
+        if (is_length_ok) && (substr = pattern) then
           let new_str =
             String.concat ""
-              [ String.sub str 0 match_start_pos;
+              [ String.sub str ~pos:0 ~len:match_start_pos;
                 replacement;
-                String.sub str (match_start_pos + pattern_len) (len - match_start_pos - pattern_len)
+                String.sub str ~pos:(match_start_pos + pattern_len) ~len:(len - match_start_pos - pattern_len)
               ]
           in
           Printf.printf "Match found: %s -> %s\n" pattern replacement;
           Printf.printf "New string: %s\n" new_str;
           let next_pos =
             if direction = 1 then
-                String.length new_str - 1
+              String.length new_str - 1
             else
-                0
+              0
           in
-          replace_substring new_str next_pos (-direction) (nb_replacements + 1)
+          replace_substring new_str next_pos (-direction) nb_replacements
         else
           find_and_replace rest
     in
     Printf.printf "Processing position: %d\n" start_pos;
     find_and_replace replacement_map
-
 
 let find_first_and_last_digits str =
   let len = String.length str in
@@ -89,19 +89,20 @@ let find_first_and_last_digits str =
 
 let rec iterate_over_list lst counter = 
   match lst with 
-  | [] -> Printf.printf "Somme: %d\n" counter
+  | [] -> Printf.printf "Sum: %d\n" counter
   | head :: tail ->
     let concat = (match find_first_and_last_digits head with
-    | Some (first_digit, last_digit) -> 
-        let digit_string = Fmt.str "%d%d" first_digit last_digit in
-        int_of_string digit_string
-    | None -> 0) in
+      | Some (first_digit, last_digit) -> 
+          let digit_string = Fmt.str "%d%d" first_digit last_digit in
+          int_of_string digit_string
+      | None -> 0) in
     let new_total = counter + concat in
     iterate_over_list tail new_total
     
 let () =
-    input 
-    |> String.split_lines
-    |> List.map (fun code -> replace_substring code 0 1 0)
-    |> iterate_over_list 0
-    |> Printf.printf "Sum: %d\n"
+    replace_substring input[0] 0 1 0 |> Printf.printf "Somme: %d\n"
+  (* input  *)
+  (* |> String.split_lines *)
+  (* |> List.map (fun code -> replace_substring code 0 1 0) *)
+  (* |> iterate_over_list 0 *)
+
